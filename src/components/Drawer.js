04b8d65045bf79871cd142a02e85d3b531a3ca16
Charlene Reilly <charlener83@hotmail.com>
2018-10-04 11:04:54 +0100

    Changed Drawer to Responsive Drawer

--- src/components/Drawer.js
+++ src/components/Drawer.js
@@ -2,43 +2,69 @@ import React from "react";
 import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
 import Drawer from "@material-ui/core/Drawer";
-import Button from "@material-ui/core/Button";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
 import List from "@material-ui/core/List";
+import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import Hidden from "@material-ui/core/Hidden";
 import Divider from "@material-ui/core/Divider";
+import MenuIcon from "@material-ui/icons/Menu";
 
-const styles = {
-  list: {
-    width: 250
+const drawerWidth = 240;
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+    height: 440,
+    zIndex: 1,
+    overflow: "hidden",
+    position: "relative",
+    display: "flex",
+    width: "100%"
   },
-  fullList: {
-    width: "auto"
+  appBar: {
+    position: "absolute",
+    marginLeft: drawerWidth,
+    [theme.breakpoints.up("md")]: {
+      width: `calc(100% - ${drawerWidth}px)`
+    }
+  },
+  navIconHide: {
+    [theme.breakpoints.up("md")]: {
+      display: "none"
+    }
+  },
+  toolbar: theme.mixins.toolbar,
+  drawerPaper: {
+    width: drawerWidth,
+    [theme.breakpoints.up("md")]: {
+      position: "relative"
+    }
+  },
+  content: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.default,
+    padding: theme.spacing.unit * 3
   }
-};
+});
 
-class TemporaryDrawer extends React.Component {
+class ResponsiveDrawer extends React.Component {
   state = {
-    left: false
+    mobileOpen: false
   };
 
-  toggleDrawer = (side, open) => () => {
-    this.setState({
-      [side]: open
-    });
+  handleDrawerToggle = () => {
+    this.setState(state => ({ mobileOpen: !state.mobileOpen }));
   };
 
   render() {
-    const { classes } = this.props;
+    const { classes, theme } = this.props;
 
-    const sideList = (
-      <div className={classes.list}>
-        <List />
+    const drawer = (
+      <div>
+        <div className={classes.toolbar} />
         <Divider />
-        <List />
-      </div>
-    );
-
-    const fullList = (
-      <div className={classes.fullList}>
         <List />
         <Divider />
         <List />
@@ -46,29 +72,63 @@ class TemporaryDrawer extends React.Component {
     );
 
     return (
-      <div>
-        <Button onClick={this.toggleDrawer("left", true)}>Open Left</Button>
-
-        <Drawer
-          open={this.state.left}
-          onClose={this.toggleDrawer("left", false)}
-        >
-          <div
-            tabIndex={0}
-            role="button"
-            onClick={this.toggleDrawer("left", false)}
-            onKeyDown={this.toggleDrawer("left", false)}
+      <div className={classes.root}>
+        <AppBar className={classes.appBar}>
+          <Toolbar>
+            <IconButton
+              color="inherit"
+              aria-label="Open drawer"
+              onClick={this.handleDrawerToggle}
+              className={classes.navIconHide}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="title" color="inherit" noWrap>
+              Responsive drawer
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Hidden mdUp>
+          <Drawer
+            variant="temporary"
+            anchor={theme.direction === "rtl" ? "right" : "left"}
+            open={this.state.mobileOpen}
+            onClose={this.handleDrawerToggle}
+            classes={{
+              paper: classes.drawerPaper
+            }}
+            ModalProps={{
+              keepMounted: true // Better open performance on mobile.
+            }}
           >
-            {sideList}
-          </div>
-        </Drawer>
+            {drawer}
+          </Drawer>
+        </Hidden>
+        <Hidden smDown implementation="css">
+          <Drawer
+            variant="permanent"
+            open
+            classes={{
+              paper: classes.drawerPaper
+            }}
+          >
+            {drawer}
+          </Drawer>
+        </Hidden>
+        <main className={classes.content}>
+          <div className={classes.toolbar} />
+          <Typography noWrap>
+            {"You think water moves fast? You should see ice."}
+          </Typography>
+        </main>
       </div>
     );
   }
 }
 
-TemporaryDrawer.propTypes = {
-  classes: PropTypes.object.isRequired
+ResponsiveDrawer.propTypes = {
+  classes: PropTypes.object.isRequired,
+  theme: PropTypes.object.isRequired
 };
 
-export default withStyles(styles)(TemporaryDrawer);
+export default withStyles(styles, { withTheme: true })(ResponsiveDrawer);

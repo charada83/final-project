0f308874724ede4964939222d4aab82de423be3f
Charlene Reilly <charlener83@hotmail.com>
2018-10-16 22:09:29 +0100

    Added invite dialog

--- src/components/Dialogs/FamilyInvite.js
+++ src/components/Dialogs/FamilyInvite.js
@@ -1,14 +1,107 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
+import Button from "@material-ui/core/Button";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import AddIcon from "@material-ui/icons/Add";
+import TextField from "@material-ui/core/TextField";
+import { withStyles } from "@material-ui/core/styles";
+
+const styles = theme => ({
+  FormControl: {
+    width: 300
+  }
+});
 
 class FamilyInvite extends Component {
-  //state = {  }
+  state = {
+    open: false,
+    inviteDetails: {
+      name: "",
+      email: ""
+    }
+  };
+
+  handleToggle = () => {
+    this.setState({
+      open: !this.state.open
+    });
+  };
+
+  handleChange = name => ({ target: { value } }) => {
+    this.setState({
+      babyDetails: {
+        ...this.state.inviteDetails,
+        [name]: value
+      }
+    });
+  };
   render() {
+    const {
+        open,
+        inviteDetails: { name, email }
+      } = this.state,
+      { classes } = this.props;
     return (
-      <div>
+      <Fragment>
         <h1>Family Invite Dialog</h1>
-      </div>
+        <Button
+          style={{ marginBottom: 20 }}
+          variant="contained"
+          onClick={this.handleToggle}
+          color="primary"
+          mini
+        >
+          <AddIcon /> Send Invite
+        </Button>
+        <Dialog
+          open={open}
+          onClose={this.handleToggle}
+          aria-labelledby="form-dialog-title"
+        >
+          <DialogTitle id="form-dialog-title">
+            Invite Family/Friends
+          </DialogTitle>
+          <DialogContent>
+            <DialogContentText>Please fill out form below</DialogContentText>
+            <form>
+              <TextField
+                name="name"
+                label="Name"
+                value={name}
+                onChange={this.handleChange("name")}
+                margin="normal"
+                className={classes.FormControl}
+              />
+              <br />
+              <TextField
+                name="email"
+                label="Email"
+                value={email}
+                onChange={this.handleChange("email")}
+                margin="normal"
+                className={classes.FormControl}
+              />
+            </form>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={this.handleToggle} color="primary">
+              Cancel
+            </Button>
+            <Button
+              /*onClick={this.handleSubmit}*/
+              color="primary"
+              variant="raised"
+            >
+              Send Invite
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </Fragment>
     );
   }
 }
 
-export default FamilyInvite;
+export default withStyles(styles)(FamilyInvite);

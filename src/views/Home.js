c38a3506a7ec49fd6a09f208967ec407818f49e1
Charlene Reilly <charlener83@hotmail.com>
2018-11-05 22:08:30 +0000

    Added commented code for dealing with users. Need to look at further

--- src/views/Home.js
+++ src/views/Home.js
@@ -2,18 +2,35 @@ import React, { Component } from "react";
 import { auth } from "../firebase";
 import Login from "../components/auth/Login";
 import { Redirect } from "@reach/router";
+import { database } from "../firebase";
+import pick from "lodash/pick";
 
 class Home extends Component {
   constructor(props) {
     super(props);
+    this.usersRef = null;
+    this.currentUserRef = null;
     this.state = {
-      currentUser: null
+      currentUser: null,
+      users: {}
     };
   }
 
   componentDidMount() {
     auth.onAuthStateChanged(currentUser => {
       this.setState({ currentUser });
+      //   this.usersRef = database.ref("/users");
+      //   this.currentUserRef = this.usersRef.child(currentUser.uid);
+
+      //   this.currentUserRef.once("value").then(snapshot => {
+      //     if (snapshot.val()) return;
+      //     const user = pick(currentUser, ["displayName", "photoURL", "email"]);
+      //     this.currentUserRef.set(user);
+      //   });
+
+      //   this.usersRef.on("value", snapshot => {
+      //     this.setState({ users: snapshot.val() });
+      //   });
     });
   }
 
